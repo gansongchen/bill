04348c001830992a5c721d44d6ab24ba23ca3f0d
gansong.chen <chengs@xencio.com>
2023-03-03 15:58:53 +0800

    init

---
+++ src/main/java/com/gansong/bill/BillApplication.java
@@ -0,0 +1,18 @@
+package com.gansong.bill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ganSong.chen
+ * @description
+ * @date 2023/3/3
+ */
+@SpringBootApplication
+public class BillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillApplication.class, args);
+    }
+
+}
